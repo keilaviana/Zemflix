714f40a9dc74b603ad65c2d858363cee87e52281
Keila Viana <keilavg72@gmail.com>
2020-07-28 09:56:30 -0300

    adicionando o inicio de cadastro

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,7 +7,18 @@ function CadastroCategoria(){
       <PageDafault>
         <h1>Página de Cadastro de Categoria </h1>
 
+        <form>
 
+          <label>
+            Nome da Categoria
+
+            <input type="text"/>
+          </label>
+
+          <button>
+            Cadastrar
+          </button>
+        </form>
 
 
         <Link to='/'>
